749acbda8ae5fbc0f1fc234e340255b205427304
LivingMartialWay <LivingMartialWay@gmail.com>
2020-06-19 17:55:10 +0300

    5th

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -65,7 +65,7 @@ function Projects() {
         params={("particles-js",
             {"particles":{
               "number":{"value":80,"density":{"enable":true,"value_area":800}},
-              "color":{"value":"#ffffff"},
+              "color":{"value":"#FFFFFF"},
               "shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},
               "opacity":{"value":0.4,"random":false,"anim":{"enable":true,"speed":1,"opacity_min":0.6,"sync":false}},
               "size":{"value":3,"random":true,"anim":{"enable":true,"speed":18,"size_min":0.1,"sync":false}},
@@ -75,7 +75,7 @@ function Projects() {
               "onhover":{"enable":true,"mode":"grab"},
               "onclick":{"enable":true,"mode":"push"},"resize":true},
               "modes":{
-                "grab":{"distance":200,"line_linked":{"opacity":1,"color":"#ff0000"}},
+                "grab":{"distance":200,"line_linked":{"opacity":1,"color":"#FFFFFF"}},
                 "bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},
                 "repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},
                 "remove":{"particles_nb":2}}},"retina_detect":true}
